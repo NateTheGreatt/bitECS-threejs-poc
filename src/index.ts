7476eca5bb1ff469f3f962d473260f388851e52a
Nate <mrtn.nathaniel@gmail.com>
2021-10-21 18:03:30 -0700

    child mesh rotation

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,9 @@ import { TransformComponent } from "./components"
 let camera: THREE.PerspectiveCamera, 
     scene: THREE.Scene, 
     renderer: THREE.WebGLRenderer,
-    mesh: THREE.Mesh
-
-init()
-animate()
+    mesh1: THREE.Mesh,
+    mesh2: THREE.Mesh,
+    mesh3: THREE.Mesh
 
 function init() {
 
@@ -20,38 +19,46 @@ function init() {
   const geometry = new THREE.BoxGeometry( 200, 200, 200 )
   const material = new THREE.MeshBasicMaterial( { wireframe: true } )
 
-  mesh = new THREE.Mesh(geometry, material)
+  mesh1 = new THREE.Mesh(geometry, material)
+  mesh2 = new THREE.Mesh(new THREE.BoxGeometry( 20, 20, 20 ),material)
+  mesh3 = new THREE.Mesh(new THREE.BoxGeometry( 100, 100, 100 ),material)
 
-  mesh.add(new THREE.Mesh(new THREE.BoxGeometry( 20, 20, 20 ),material))
-  mesh.add(new THREE.Mesh(new THREE.BoxGeometry( 100, 100, 100 ),material))
+  const meshes = [mesh1,mesh2,mesh3]
+
+  mesh1.add(mesh2)
+  mesh1.add(mesh3)
 
   const world = createWorld()
 
-  const e = addEntity(world)
-
   const {rotation} = TransformComponent
 
-  Object.defineProperty(mesh.rotation, 'eid', { get: () => e })
-  Object.defineProperty(mesh.rotation, 'store', { get: () => rotation })
-  Object.defineProperty(mesh.rotation, '_x', {
-    get () { return this.store.x[this.eid] },
-    set (n) { this.store.x[this.eid] = n }
-  })
-  Object.defineProperty(mesh.rotation, '_y', {
-    get () { return this.store.y[this.eid] },
-    set (n) { this.store.y[this.eid] = n }
-  })
-  Object.defineProperty(mesh.rotation, '_z', {
-    get () { return this.store.z[this.eid] },
-    set (n) { this.store.z[this.eid] = n }
-  })
+  for (let i = 0; i < 3; i++) {
+    const eid = addEntity(world)
+    const mesh = meshes[i]
+    
+    Object.defineProperty(mesh.rotation, 'eid', { get: () => eid })
+    Object.defineProperty(mesh.rotation, 'store', { get: () => rotation })
+
+    Object.defineProperty(mesh.rotation, '_x', {
+      get () { return this.store.x[this.eid] },
+      set (n) { this.store.x[this.eid] = n }
+    })
+    Object.defineProperty(mesh.rotation, '_y', {
+      get () { return this.store.y[this.eid] },
+      set (n) { this.store.y[this.eid] = n }
+    })
+    Object.defineProperty(mesh.rotation, '_z', {
+      get () { return this.store.z[this.eid] },
+      set (n) { this.store.z[this.eid] = n }
+    })
+  }
 
   renderer = new THREE.WebGLRenderer( { antialias: true } )
   renderer.setPixelRatio( window.devicePixelRatio )
   renderer.setSize( window.innerWidth, window.innerHeight )
   document.body.appendChild( renderer.domElement )
 
-  scene.add( mesh )
+  scene.add( mesh1 )
 
   window.addEventListener( 'resize', onWindowResize )
 
@@ -70,11 +77,19 @@ function animate() {
 
   requestAnimationFrame( animate )
 
-  mesh.rotation.x += 0.005
+  mesh1.rotation.x += 0.005
   TransformComponent.rotation.y[0] += 0.01
 
-  mesh.rotation._onChangeCallback()
+  mesh2.rotation.z += -0.1
+
+  mesh3.rotation.y += -0.1
+
+  mesh1.rotation._onChangeCallback()
 
   renderer.render( scene, camera )
 
 }
+
+
+init()
+animate()
