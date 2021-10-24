0590190e37930574bb2c0e68c90710693d580b48
Nate <mrtn.nathaniel@gmail.com>
2021-10-23 23:27:19 -0700

    attachments

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,22 @@ import { pipeline } from "./pipeline"
 
 const world = createWorld()
 
-for (let i = 1; i <= 3; i++) {
-  const size = 50 * i
+
+const parent = createObject3DEntity(world, new THREE.BoxGeometry(100,100,100))
+addComponent(world, TransformComponent, parent.eid)
+world.objects.set(parent.eid, parent)
+world.scene.add(parent)
+
+for (let i = 1; i <= 10; i++) {
+  const size = 10 * (10-i)
   const obj3d = createObject3DEntity(world, new THREE.BoxGeometry(size,size,size))
   addComponent(world, TransformComponent, obj3d.eid)
   world.objects.set(obj3d.eid, obj3d)
-  world.scene.add(obj3d)
+  let last = world.objects.get(obj3d.eid-1)
+  if (!last) parent.add(obj3d)
+  else last.add(obj3d)
 }
+
 
 const update = () => {
 
--- src/system/movement.ts
+++ src/system/movement.ts
@@ -5,19 +5,28 @@ import { World } from "../model/World"
 export const movementQuery = defineQuery([TransformComponent])
 
 export const movementSystem = (world: World) => {
+  const { time: { delta, elapsed } } = world
   const ents = movementQuery(world)
   for (let i = 0; i < ents.length; i++) {
     const e = ents[i]
     const obj3d = world.objects.get(e)
 
-    TransformComponent.rotation.x[e] += 0.001
-    TransformComponent.rotation.y[e] += 0.03
-    TransformComponent.rotation.z[e] += 0.005
+    TransformComponent.rotation.x[e] += 0.0001 * delta
+    TransformComponent.rotation.y[e] += 0.003 * delta
+    TransformComponent.rotation.z[e] += 0.0005 * delta
     obj3d!.rotation._onChangeCallback()
 
-    TransformComponent.position.x[e] += Math.sin(world.time.elapsed/1000) / 10 * world.time.delta
-    TransformComponent.position.y[e] += Math.cos(world.time.elapsed/1000) / 10 * world.time.delta
-    TransformComponent.position.z[e] += Math.cos(world.time.elapsed/1000) / 10 * world.time.delta
+    TransformComponent.position.x[e] += Math.sin(elapsed/1000) / 30 * delta
+    TransformComponent.position.y[e] += Math.cos(elapsed/1000) / 30 * delta
+    TransformComponent.position.z[e] += Math.cos(elapsed/1000) / 30 * delta
+
+    // obj3d!.rotation.x += 0.001
+    // obj3d!.rotation.y += 0.03
+    // obj3d!.rotation.z += 0.005
+
+    // obj3d!.position.x += Math.sin(world.time.elapsed/1000) / 10 * world.time.delta
+    // obj3d!.position.y += Math.cos(world.time.elapsed/1000) / 10 * world.time.delta
+    // obj3d!.position.z += Math.cos(world.time.elapsed/1000) / 10 * world.time.delta
   }
   return world
 }
